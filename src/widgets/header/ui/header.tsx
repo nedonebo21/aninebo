b7ae1281a0f8eb7263b373fc10991c3f4b33d11e
nedonebo21 <cursed.void10007@gmail.com>
2026-03-10 15:29:44 +0300

    feat: disable auth button if pathname = '/auth'

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,12 +1,20 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 import { SearchAnime } from '@/features/search-anime'
 import { SwitchTheme } from '@/features/switch-theme'
+import { cn } from '@/shared/lib/utils'
 import { Button, Container, NavBar } from '@/shared/ui'
 
 import { HeaderLogo } from './header-logo'
 
 export const Header = () => {
+  const pathName = usePathname()
+
+  const isOnAuthPage = pathName === '/auth'
+
   return (
     <header className={'border-b'}>
       <Container className={'flex items-center justify-between py-4 gap-4'}>
@@ -14,8 +22,15 @@ export const Header = () => {
         <SearchAnime />
         <NavBar />
         <div className={'flex items-center gap-4'}>
-          <Link href={'/auth'}>
-            <Button className={'cursor-pointer'}>Авторизация</Button>
+          <Link
+            href={'/auth'}
+            className={cn({
+              'pointer-events-none': isOnAuthPage,
+            })}
+          >
+            <Button className={cn({ 'cursor-pointer': !isOnAuthPage })} disabled={isOnAuthPage}>
+              Авторизация
+            </Button>
           </Link>
           <SwitchTheme />
         </div>
