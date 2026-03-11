c14d6f8fc0c8e6c89f0d8e21dca51e6cbe7501ef
nedonebo21 <cursed.void10007@gmail.com>
2026-03-11 13:38:51 +0300

    feat: add typography

---
+++ src/shared/ui/typography/typography.tsx
@@ -0,0 +1,46 @@
+import { cn } from '@/shared/lib/utils'
+
+import type { ComponentProps, ElementType, ReactNode } from 'react'
+
+const variantMapping = {
+  title: 'text-4xl font-extrabold leading-tight',
+  subtitle: 'text-[22px] font-bold leading-tight',
+  bodyNormal: 'text-base font-normal leading-tight',
+  bodySemiBold: 'text-[15px] font-semibold leading-tight',
+  bodyBold: 'text-base font-bold leading-tight',
+  error: 'text-red-600 text-sm',
+}
+
+type TypographyOwnProps<T extends ElementType = ElementType> = {
+  as?: T
+  children: ReactNode
+  noWrap?: boolean
+  variant?: keyof typeof variantMapping
+  textAlign?: 'left' | 'center' | 'right' | 'justify'
+}
+
+type TypographyProps<T extends ElementType> = TypographyOwnProps<T> &
+  Omit<ComponentProps<T>, keyof TypographyOwnProps>
+
+export const Typography = <T extends ElementType = 'p'>({
+  as,
+  className,
+  variant = 'bodyNormal',
+  noWrap,
+  textAlign = 'left',
+  ...rest
+}: TypographyProps<T>) => {
+  const classes = cn(
+    variantMapping[variant],
+    textAlign === 'center' && 'text-center',
+    textAlign === 'left' && 'text-left',
+    textAlign === 'right' && 'text-right',
+    textAlign === 'justify' && 'text-justify',
+    noWrap && 'whitespace-nowrap',
+    className
+  )
+
+  const Component = as ?? 'p'
+
+  return <Component className={classes} {...rest} />
+}
