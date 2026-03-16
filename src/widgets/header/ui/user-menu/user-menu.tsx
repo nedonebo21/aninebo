d20e36245b95eb8471d1d2d15f0aae4ebc598190
nedonebo21 <cursed.void10007@gmail.com>
2026-03-16 13:26:01 +0300

    feat: add user-menu

---
+++ src/widgets/header/ui/user-menu/user-menu.tsx
@@ -0,0 +1,39 @@
+import {
+  Button,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@/shared/ui'
+import Link from 'next/link'
+import { LogoutButton } from '@/features/auth'
+
+type UserMenuProps = {
+  id?: number
+  avatar?: string
+  nickname?: string
+}
+export const UserMenu = ({ id, avatar, nickname }: UserMenuProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={'ghost'} size={'icon'}>
+          <img className={'rounded-md'} src={avatar} alt={'avatar'} />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuGroup className={'grid gap-1'}>
+          <DropdownMenuLabel>{nickname}</DropdownMenuLabel>
+          <DropdownMenuItem>
+            <Link href={`profile/${id}`}>Профиль</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <LogoutButton />
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
