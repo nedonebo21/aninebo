585fe0abf9d8f1cfde5678d7e10b971109937cdf
nedonebo21 <cursed.void10007@gmail.com>
2026-03-17 10:21:56 +0300

    refactor: tailwind cfg for dynamic classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,4 +2,6 @@ import type { Config } from 'tailwindcss'
 
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+  // @ts-ignore
+  safelist: ['bg-green-500', 'bg-gray-400', 'text-violet-500', 'bg-violet-500'],
 } satisfies Config
