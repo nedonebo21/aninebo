7df891085b0ae87637659d2af3c6b0f19db0b14f
nedonebo21 <cursed.void10007@gmail.com>
2026-03-19 11:11:17 +0300

    fix: axios instance

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
-const isServer = typeof window === 'undefined'
-
 export const axiosInstance = axios.create({
-  baseURL: isServer ? process.env.NEXT_PUBLIC_API_URL : process.env.NEXT_PUBLIC_CLIENT_URL,
+  baseURL: process.env.NEXT_PUBLIC_CLIENT_URL,
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
@@ -17,7 +15,7 @@ axiosInstance.interceptors.request.use(
       config.headers['X-Application'] = process.env.NEXT_PUBLIC_API_TOKEN
     }
 
-    if (!isServer) {
+    if (typeof window !== 'undefined') {
       const token = localStorage.getItem('token')
       if (token) {
         config.headers.Authorization = `Bearer ${token}`
