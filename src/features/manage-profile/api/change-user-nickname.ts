b8bcc5230bc4214f12e28f3f2eb4b42a40174976
nedonebo21 <cursed.void10007@gmail.com>
2026-03-19 13:05:38 +0300

    feat: PATCH request for change nickname

---
+++ src/features/manage-profile/api/change-user-nickname.ts
@@ -0,0 +1,26 @@
+import { axiosInstance } from '@/shared/api'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import toast from 'react-hot-toast'
+
+const changeUserNickname = async ({ id, nickname }: { id: number; nickname?: string }) => {
+  return (
+    await axiosInstance.patch(`/users/${id}`, {
+      nickname,
+    })
+  ).data
+}
+
+export function useChangeUserNickname() {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: changeUserNickname,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['me'] })
+      toast.success('Данные успешно изменены')
+    },
+    onError: err => {
+      console.error(err)
+      toast.error('Ошибка смены данных')
+    },
+  })
+}
