123940e35be44cf3f0dbc66363d2e1d38512a4a2
nedonebo21 <cursed.void10007@gmail.com>
2026-03-19 13:06:24 +0300

    feat: add controlled textarea

---
+++ src/shared/forms/controlled-textarea.tsx
@@ -0,0 +1,63 @@
+import { Control, FieldValues, useController, UseControllerProps } from 'react-hook-form'
+import { ClearButton, Input, RequiredSymbol, Textarea, Typography } from '@/shared/ui'
+
+type ControlledTextareaProps<T extends FieldValues> = {
+  label?: string
+  required?: boolean
+  className?: string
+  placeholder?: string
+  errorMessage?: string
+  control: Control<T>
+} & Omit<UseControllerProps<T>, 'control'>
+
+export const ControlledTextarea = <T extends FieldValues>({
+  label,
+  required,
+  className,
+  errorMessage,
+  name,
+  rules,
+  control,
+  ...rest
+}: ControlledTextareaProps<T>) => {
+  const {
+    field: { onChange, onBlur, value },
+  } = useController({
+    name,
+    rules,
+    control,
+    ...rest,
+  })
+
+  const handleClear = () => {
+    onChange('')
+  }
+
+  const isError = !!errorMessage
+
+  const isClearable = !!value
+
+  return (
+    <label className={className}>
+      {label && (
+        <Typography
+          as={'p'}
+          variant={'bodyNormal'}
+          className={'flex gap-1 text-sm font-medium mb-1'}
+        >
+          {label}
+          {required && <RequiredSymbol />}
+        </Typography>
+      )}
+      <div className={'relative'}>
+        <Textarea className={'h-10 text-md'} {...{ ...rest, value, onChange, onBlur, id: name }} />
+        {isClearable && <ClearButton onClick={handleClear} />}
+      </div>
+      {isError && (
+        <Typography variant={'error'} as={'p'} className={'text-sm mt-2'}>
+          {errorMessage}
+        </Typography>
+      )}
+    </label>
+  )
+}
