6754b5d5f6e19bad4562ada632f156be88b8f923
nedonebo21 <cursed.void10007@gmail.com>
2026-03-24 02:28:36 +0300

    refactor: extract manage-profile logic into custom hook

---
+++ src/features/manage-profile/model/hooks/index.ts
@@ -0,0 +1,1 @@
+export { useProfileForm } from './use-profile-form'
---
+++ src/features/manage-profile/model/hooks/use-profile-form.ts
@@ -0,0 +1,55 @@
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { useMe } from '@/entities/user'
+import { useChangeUserNickname, useChangeProfile } from '@/features/manage-profile/api'
+import type { ChangeProfileFormValues } from '../change-profile-schema'
+import { changeProfileSchema } from '../change-profile-schema'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+export const useProfileForm = () => {
+  const { data: userData } = useMe()
+  const { mutate: changeUserNickname } = useChangeUserNickname()
+  const { mutate: changeUserInfo } = useChangeProfile()
+
+  const {
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors, dirtyFields },
+  } = useForm<ChangeProfileFormValues>({
+    resolver: zodResolver(changeProfileSchema),
+    defaultValues: {
+      nickname: userData?.response.nickname ?? '',
+      about: userData?.response.about ?? '',
+    },
+  })
+
+  useEffect(() => {
+    if (userData?.response) {
+      reset({
+        nickname: userData.response.nickname ?? '',
+        about: userData.response.about ?? '',
+      })
+    }
+  }, [userData, reset])
+
+  const handleProfileUpdate = (data: ChangeProfileFormValues) => {
+    const id = userData?.response.id
+    if (!id) return
+
+    if (dirtyFields.nickname) {
+      changeUserNickname({ id, nickname: data.nickname ?? '' })
+    }
+
+    if (dirtyFields.about) {
+      changeUserInfo({ about: data.about ?? '' })
+    }
+  }
+
+  return {
+    control,
+    errors,
+    handleSubmit,
+    handleProfileUpdate,
+  }
+}
