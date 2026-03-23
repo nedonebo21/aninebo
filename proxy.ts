0e4ae6978d13e3c3d90efc618c4e940fea5a6f06
nedonebo21 <cursed.void10007@gmail.com>
2026-03-23 17:26:46 +0300

    refactor: rename middleware on proxy

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   if (req.nextUrl.pathname.startsWith('/api/yani')) return NextResponse.next()
 
   const token = req.cookies.get('yummy_token')?.value
