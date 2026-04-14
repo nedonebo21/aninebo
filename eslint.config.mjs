fd9e404941ecdd72ae777431d8d2a02d57616255
nedonebo21 <cursed.void10007@gmail.com>
2026-04-14 14:16:05 +0300

    fix: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,38 @@
 import baseConfig from '@nedonebo21/eslint-config'
 
+const configWithoutDuplicateTsPlugin = baseConfig.map(configItem => {
+  const plugins = configItem.plugins
+
+  if (!plugins || !plugins['@typescript-eslint']) {
+    return configItem
+  }
+
+  const hasTypescriptRules = Object.keys(configItem.rules ?? {}).some(ruleName =>
+    ruleName.startsWith('@typescript-eslint/')
+  )
+
+  if (hasTypescriptRules) {
+    return configItem
+  }
+
+  const restPlugins = { ...plugins }
+  delete restPlugins['@typescript-eslint']
+
+  if (Object.keys(restPlugins).length === 0) {
+    const restConfigItem = { ...configItem }
+    delete restConfigItem.plugins
+
+    return restConfigItem
+  }
+
+  return {
+    ...configItem,
+    plugins: restPlugins,
+  }
+})
+
 const config = [
-  ...baseConfig,
+  ...configWithoutDuplicateTsPlugin,
   {
     rules: {
       '@next/next/no-img-element': 'off',
